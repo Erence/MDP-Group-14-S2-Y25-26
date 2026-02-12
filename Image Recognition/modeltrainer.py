8af42f20a4f590c4eabb2bb97adb925ab6ffaeba
caleb608 <CYEO039@e.ntu.edu.sg>
2026-02-13 06:36:36 +0800

    caleb-imagerec-part2: Added seg_v2 model
    Trained 50 epochs on Pante's dataset downloaded with YOLOv8 Bounding Boxes. Noticed a few missing labels in the dataset, so may not be the most accurate. Working on getting label-studio to work.

--- Image Recognition/modeltrainer.py
+++ Image Recognition/modeltrainer.py
@@ -12,9 +12,10 @@ model = YOLO("yolo26n-seg.pt") # Latest nano instance segmentation model
 new_path = os.path.relpath("C:/Users/Caleb/Documents/MDP Image Recognition/Data/YuktoSC/data.yaml")
 
 def main():
-    results = model.train(data=new_path, 
-                          name="seg_v2", 
-                          epochs=50, 
+    results = model.train(data=new_path,
+                          project="Image Recognition/runs",
+                          name="seg_v3", 
+                          epochs=200, 
                           imgsz=640, 
                           device=device,
                           patience=25,
