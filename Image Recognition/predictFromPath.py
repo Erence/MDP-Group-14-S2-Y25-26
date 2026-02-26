71c7f9f223c68af07d1e7b8f978687517cf61e6e
caleb608 <CYEO039@e.ntu.edu.sg>
2026-02-26 16:04:16 +0800

    caleb-imagerec-part5: Added seg_v4
    Trained on dataset excluding older images with black background and coloured symbol.

--- Image Recognition/predictFromPath.py
+++ Image Recognition/predictFromPath.py
@@ -2,13 +2,15 @@ from ultralytics import YOLO
 import cv2
 import os
 
-model = YOLO("Image Recognition/runs/seg_v2.pt")
+model = YOLO("Image Recognition/runs/seg_v4.pt")
 
-imgpath1 = os.path.relpath("C:/Users/caleb/Documents/MDP Image Recognition/Data/YuktoSC/test/images/20240126_091022_jpg.rf.8b922c5702bb61149742583d22e84583.jpg")
-imgpath2 = os.path.relpath("C:/Users/caleb/Documents/MDP Image Recognition/Data/YuktoSC/test/images/20240211_232039_jpg.rf.8ffbadc50a357c89d63a377e69e430ac.jpg")
+imgpath1 = os.path.relpath("C:/Users/Caleb/Documents/MDP Image Recognition/Data/rawImages/library_stony_3.jpg")
+imgpath2 = os.path.relpath("C:/Users/Caleb/Documents/MDP Image Recognition/Data/rawImages/photo_2026-02-25_16-01-55.jpg")
+imgpath3 = os.path.relpath("C:/Users/Caleb/Documents/MDP Image Recognition/Data/MDP.v7i/test/images/IMG_2480_jpg.rf.13c46008317bb802229122f8d9f7dafc.jpg")
+imgpath4 = os.path.relpath("C:/Users/Caleb/Documents/MDP Image Recognition/Data/MDP.v7i/test/images/IMG_2371_jpg.rf.868ead450d8a03304e7389f6eed837a8.jpg")
 
 def predict():
-    results = model.predict(source=[imgpath1, imgpath2], show = True)
+    results = model.predict(source=[imgpath1, imgpath2, imgpath3, imgpath4], show = True)
     for r in results:
         clss_list = r.boxes.cls.int().tolist()
         for cls in clss_list:
